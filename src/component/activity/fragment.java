4039a00fb4e7497fa6a2ec7a0054590575da2bed
zhangjl <786069240@qq.com>
2015-07-12 18:40:22 +0800

    activity and fragment

---
+++ src/component/activity/fragment.java
@@ -0,0 +1,6 @@
+// fragment
+public static class ExampleFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.example_fragment, container, false);
+    }
+}
